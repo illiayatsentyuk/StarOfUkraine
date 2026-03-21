2a259c39c3642f38f623053a14be4bb4f9aa1678
illiayatsentyuk <illiayatsentyuk@gmail.com>
2026-03-21 18:19:10 +0200

    Refactor frontend project structure to migrate from Vite to Nuxt: removed Vite-specific configurations, updated package.json scripts for Nuxt, and cleaned up unused files and dependencies. Enhanced .gitignore for better environment management and added new dependencies for Nuxt integration.

---
+++ apps/frontend/app/app.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div
+    NuxtRouteAnnouncer
+    NuxtWelcome
+</template>
+
+<script setup lang="ts">
+const counterStore = useCounterStore();
+
+onMounted(() => {
+  counterStore.increment();
+  console.log(counterStore.count);
+});
+</script>
---
+++ apps/frontend/nuxt.config.ts
@@ -0,0 +1,34 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@pinia/nuxt',
+    '@primevue/nuxt-module',
+    '@vee-validate/nuxt'
+  ],
+  css: ["~/assets/styles/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/assets/styles/variables.css" as *;`,
+        },
+      },
+    },
+  },
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+  devServer: {
+    port:4040,
+  },  
+  pinia: {
+    storesDirs: ['stores'],
+  },
+})
