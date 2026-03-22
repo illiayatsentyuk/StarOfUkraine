865f145b7d468e6c796ad4486773e76b30d61ff1
illiayatsentyuk <illiayatsentyuk@gmail.com>
2026-03-22 13:18:12 +0200

    Enable CORS in main.ts to allow requests from any origin

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,7 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: '*',
+  });
   const config = new DocumentBuilder()
     .setTitle('StarOfUkraine API')
     .setDescription('API documentation for auth, teams, and tournaments')
