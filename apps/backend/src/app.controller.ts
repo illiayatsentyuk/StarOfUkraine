c827f1597901388cba3e84d312598fd90d668855
illiayatsentyuk <illiayatsentyuk@gmail.com>
2026-03-26 09:26:56 +0200

    Update API responses in AppController and AppService: modified the getHello method to return a structured JSON response with status, message, and version information instead of a simple string. Enhanced API documentation to reflect these changes.

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -14,9 +14,13 @@ export class AppController {
   @ApiResponse({
     status: 200,
     description: 'Service is reachable',
-    schema: { example: 'Hello World!' },
+    schema: { example: {
+      "ok": true,
+      "message": "Server is running",
+      "version": "1.0.0",
+    } },
   })
-  getHello(): string {
+  getHello() {
     return this.appService.getHello()
   }
 }
--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!'
+  getHello() {
+    return {
+      "ok": true,
+      "message": "Server is running",
+      "version": "1.0.0",
+    }
   }
 }
