be73ec780722f60b86e1ead04a66bd07536224ad
Maf1ck <kosmanmatvij@gmail.com>
2026-04-11 15:41:13 +0300

    feat: implement tournament detail page with bracket view and team management integration

--- apps/frontend/app/pages/tournaments/[id].vue
+++ apps/frontend/app/pages/tournaments/[id].vue
@@ -186,6 +186,7 @@ import TournamentTeamsTable from '../../components/tournaments/TournamentTeamsTa
 import TeamsCreateTeamModal from '../../components/Teams/CreateTeamModal.vue'
 import DeleteModal from '../../components/tournaments/deleteModal.vue'
 import { useTournamentBracket } from '../../stores/tournamentBrackets.store'
+import { useLocale } from '~/composables/useLocale'
 
 import { calculateTournamentStatus } from '../../utils/tournament-status'
 
@@ -195,6 +196,7 @@ const authStore = useLoginStore()
 const teamsStore = useTeamsStore()
 const bracketStore = useTournamentBracket()
 
+const locale = useLocale()
 
 const tournament = ref<any>(null)
 const teams = ref<any[]>([])
