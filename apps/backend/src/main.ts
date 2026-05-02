0af5d790dcc47cb6822c3a8c826baff903769663
illiayatsentyuk <illiayatsentyuk@gmail.com>
2026-05-02 12:13:30 +0300

    Update CORS configuration in main.ts to allow requests from both localhost and production frontend URL

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   );
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:4040',
+    origin: ['http://localhost:4040','https://star-of-ukraine-frontend.vercel.app'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
